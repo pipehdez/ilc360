ebcf0175cf832afae972f01c1a99933fd31d915c
andreshernandez-lafise <151097543+andreshernandez-lafise@users.noreply.github.com>
2024-07-08 23:40:21 -0500

    fix video

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -29,9 +29,7 @@ export default function Video() {
             <iframe
               src="https://www.youtube-nocookie.com/embed/aOq49euWnIo?controls=0&autoplay=1"
               title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen></iframe>
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
           )}
         </div>
       </div>
