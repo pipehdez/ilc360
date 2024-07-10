fd893248bc9052712825903deef06db9a0017f1a
andreshernandez-lafise <151097543+andreshernandez-lafise@users.noreply.github.com>
2024-07-10 15:00:02 -0500

    wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from '@/components/navbar'
-import { Provider } from "./provider";
-import { useRouter } from 'next/navigation'
 import Wrapper from './wrapper'
 
 const inter = Inter({ subsets: ["latin"] });
--- src/app/wrapper.tsx
+++ src/app/wrapper.tsx
@@ -5,10 +5,12 @@ import Navbar from '@/components/navbar'
 
 export default function Wrapper({ children }: Readonly<{ children: React.ReactNode }>) {
     const path = usePathname()
-    return <Provider>
+    return (
+        <Provider>
             <main className={`flex min-h-screen flex-col items-center ${path === '/' ? 'bg-hero-background-image bg-no-repeat bg-cover' : 'bg-white'}  `}>
                 <Navbar />
                 {children}
             </main>
-    </Provider>
+        </Provider>
+    )
 }
