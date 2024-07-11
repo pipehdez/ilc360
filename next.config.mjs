f7fccd8b227201a6085edf9cf16e8a063d98eaef
andreshernandez-lafise <151097543+andreshernandez-lafise@users.noreply.github.com>
2024-07-10 21:20:30 -0500

    env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        AUTH_SECRET: process.env.AUTH_SECRET,
+        NEXT_PUBLIC_MONGODB_URI: process.env.MONGODB_URI,
+        NEXT_PUBLIC_AUTH_SECRET: process.env.AUTH_SECRET,
+    }
+};
 
 export default nextConfig;
