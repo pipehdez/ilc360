c8d4d35fc91fbc843dc99153f977048ba22eb7d3
andreshernandez-lafise <151097543+andreshernandez-lafise@users.noreply.github.com>
2024-07-11 08:05:02 -0500

    fix env

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        MONGODB_URI: "mongodb+srv://dev:941001@cluster0.yqorpme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-        AUTH_SECRET: "x9cH/cpjdXDJPpMdR1P65oUm4Wzm0XV9pZm9LvzFq6U="
-    }
 };
 
 export default nextConfig;
